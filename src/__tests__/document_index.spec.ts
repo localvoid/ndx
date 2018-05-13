b5b1905fc0dcaa6cb47e156ac5de905f4cc22fa2
Boris Kaul <localvoid@gmail.com>
2018-05-13 22:26:40 +0700

    test: add more tests

--- src/__tests__/document_index.spec.ts
+++ src/__tests__/document_index.spec.ts
@@ -1,4 +1,10 @@
-import { DocumentIndex } from "../document_index";
+import { DocumentIndex } from "..";
+
+interface Doc {
+  readonly id: string;
+  readonly title: string;
+  readonly body: string;
+}
 
 const DOCS = [
   {
@@ -19,21 +25,81 @@ const DOCS = [
 ];
 
 describe("DocumentIndex", () => {
-  describe("empty", () => {
+  describe("empty index", () => {
     test("should have size 0", () => {
-      const idx = new DocumentIndex<string, { id: string, title: string, body: string }>();
+      const idx = new DocumentIndex<string, Doc>();
       expect(idx.size).toBe(0);
     });
 
     test("should perform search queries", () => {
-      const idx = new DocumentIndex<string, { id: string, title: string, body: string }>();
+      const idx = new DocumentIndex<string, Doc>();
       expect(idx.search("a")).toEqual([]);
+    });
+  });
+
+  describe("options", () => {
+    test("custom tokenizer should be invoked when indexing documents", () => {
+      let i = 0;
+      const idx = new DocumentIndex<string, Doc>({
+        tokenizer: (text) => (i++ , text.split(",")),
+      });
+      idx.addField("body");
+      idx.add("a", { id: "a", title: "", body: "a,b,c" });
+      idx.add("b", { id: "b", title: "", body: "d,e,f" });
+
+      expect(i).toBe(2);
+    });
+
+    test("custom tokenizer should be invoked when searching", () => {
+      let i = 0;
+      const idx = new DocumentIndex<string, Doc>({
+        tokenizer: (text) => (i++ , text.split(",")),
+      });
+      idx.addField("body");
+      idx.search("a b");
+
+      expect(i).toBe(1);
+    });
+
+    test("custom tokenizer should be applied when indexing documents", () => {
+      const idx = new DocumentIndex<string, Doc>({
+        tokenizer: (text) => text.split(","),
+      });
+      idx.addField("body");
+      idx.add("a", { id: "a", title: "", body: "a,b,c" });
+      idx.add("b", { id: "b", title: "", body: "d,e,f" });
+
+      const result = idx.search("b");
+      expect(result.length).toBe(1);
+      expect(result[0].docId).toBe("a");
+    });
+
+    test("custom filter should be invoked when indexing documents", () => {
+      let i = 0;
+      const idx = new DocumentIndex<string, Doc>({
+        filter: (text) => (i++ , text.toUpperCase()),
+      });
+      idx.addField("body");
+      idx.add("a", { id: "a", title: "", body: "a b c" });
+
+      expect(i).toBe(3);
+    });
+
+    test("custom filter should be invoked when searching", () => {
+      let i = 0;
+      const idx = new DocumentIndex<string, Doc>({
+        filter: (text) => (i++ , text.toUpperCase()),
+      });
+      idx.addField("body");
+      idx.search("a b");
+
+      expect(i).toBe(2);
     });
   });
 
   describe("multiple docs", () => {
     test("should index multiple fields", () => {
-      const idx = new DocumentIndex<string, { id: string, title: string, body: string }>();
+      const idx = new DocumentIndex<string, Doc>();
       idx.addField("title");
       idx.addField("body");
       DOCS.forEach((d) => {
@@ -55,8 +121,8 @@ describe("DocumentIndex", () => {
       expect(result[1].docId).toBe("c");
     });
 
-    test("should remove documents", () => {
-      const idx = new DocumentIndex<string, { id: string, title: string, body: string }>();
+    test("autovacuum should remove documents", () => {
+      const idx = new DocumentIndex<string, Doc>();
       idx.addField("title");
       idx.addField("body");
       DOCS.forEach((d) => {
@@ -69,6 +135,21 @@ describe("DocumentIndex", () => {
       expect(result[0].docId).toBe("b");
 
       result = idx.search("a");
+      expect(result.length).toBe(1);
+      expect(result[0].docId).toBe("c");
+    });
+
+    test("vacuum should remove documents", () => {
+      const idx = new DocumentIndex<string, Doc>();
+      idx.addField("title");
+      idx.addField("body");
+      DOCS.forEach((d) => {
+        idx.add(d.id, d);
+      });
+      idx.remove("a");
+      idx.vacuum();
+
+      const result = idx.search("a");
       expect(result.length).toBe(1);
       expect(result[0].docId).toBe("c");
     });
